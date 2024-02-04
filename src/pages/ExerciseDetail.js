f8878bb9a39abc22aecc4cd117947929ae2576b3
Ahmed Kamel <ahmedkamel.egy5577@gmail.com>
2024-02-04 17:32:44 +0200

    creating new components

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 const ExerciseDetail = () => {
-	return <div>ExerciseDetail</div>;
+	return (
+		<>
+			<Detail />
+			<ExerciseVideos />
+			<SimilarExercises />
+		</>
+	);
 };
 
 export default ExerciseDetail;
